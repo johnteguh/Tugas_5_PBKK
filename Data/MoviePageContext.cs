62143abe5671e381097740b52898f16b240dec37
Yohanes Teguh Ukur Ginting <yohanes.teguh36@gmail.com>
2023-12-18 02:03:07 +0700

    update

---
+++ Data/MoviePageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoviePage.Models;
+
+namespace MoviePage.Data
+{
+    public class MoviePageContext : DbContext
+    {
+        public MoviePageContext (DbContextOptions<MoviePageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoviePage.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MoviePage.Data;
+using MoviePage.Models;
+
+namespace MoviePage.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MoviePage.Data.MoviePageContext _context;
+
+        public DetailsModel(MoviePage.Data.MoviePageContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Movie = movie;
+            }
+            return Page();
+        }
+    }
+}
